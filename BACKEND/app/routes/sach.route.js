044c8820c60c569cda16d4349f5f9b1004f2af50
Minh Khang <minhkhanggr.2020@gmail.com>
2026-07-24 02:15:24 +0700

    feat(auth): áp middleware phân quyền cho 5 route

--- BACKEND/app/routes/sach.route.js
+++ BACKEND/app/routes/sach.route.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const sach = require("../controllers/sach.controller");
-
+const verifyToken = require("../middlewares/verifyToken");
+const checkRole = require("../middlewares/checkRole");
 const router = express.Router();
 
 router.route("/")
     .get(sach.findAll)
-    .post(sach.create)
-    .delete(sach.deleteAll);
+    .post(verifyToken, checkRole(["Admin"]), sach.create)
+    .delete(verifyToken, checkRole(["Admin"]), sach.deleteAll);
 
 router.route("/:id")
     .get(sach.findOne)
-    .put(sach.update)
-    .delete(sach.delete);
+    .put(verifyToken, checkRole(["Admin"]), sach.update)
+    .delete(verifyToken, checkRole(["Admin"]), sach.delete);
 
 // Route để upload ảnh
-router.post("/upload/image", sach.uploadImage);
+router.post("/upload/image", verifyToken, checkRole(["Admin"]), sach.uploadImage);
 
 module.exports = router;
